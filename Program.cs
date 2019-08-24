80e0ff7d8943e3ddefc1133e4a1317247bc83d25
Ivan Strigo <ivan@netmedia.hr>
2019-08-24 11:43:51 +0200

    adding solution

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CsharpConsoleAppLessons
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
