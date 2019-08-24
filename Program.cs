1675a427077d80918d415841c3dacc98d40fd0d1
Ivan Strigo <ivan@netmedia.hr>
2019-08-24 12:15:29 +0200

    Playground

--- Program.cs
+++ Program.cs
@@ -4,9 +4,9 @@ namespace CsharpConsoleAppLessons
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            CodeSamples.Nullable();
         }
     }
 }
---
+++ Snippets/StringType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class CodeSamples {
+
+    internal static void Nullable()
+    {
+
+        int? i = null;
+
+        int val = i ?? 0;
+
+        Console.WriteLine("i is {0}", val);
+        
+    }
+}
